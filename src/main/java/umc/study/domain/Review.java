5966677ba0db5eba9975f568b9380c6ec2e1e149
jjin70 <jjin0434@naver.com>
2023-12-05 22:47:31 +0900

    [feat] 가게에 리뷰 추가하기 API 구현

--- src/main/java/umc/study/domain/Review.java
+++ src/main/java/umc/study/domain/Review.java
@@ -4,6 +4,7 @@ import lombok.*;
 import umc.study.domain.common.BaseEntity;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
@@ -16,6 +17,12 @@ public class Review extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String title;
+
+    private Float score;
+
+    private String body;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
@@ -23,6 +30,17 @@ public class Review extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "store_id")
     private Store store;
+    public void setMember(Member member){
+        if(this.member != null)
+            member.getReviewList().remove(this);
+        this.member = member;
+        member.getReviewList().add(this);
+    }
 
-    private String body;
+    public void setStore(Store store){
+        if (this.score != null)
+            store.getReviewList().remove(this);
+        this.store = store;
+        store.getReviewList().add(this);
+    }
 }
